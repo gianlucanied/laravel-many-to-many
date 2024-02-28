adae4711077bf71e46b9fbafa18ddd2232acb99d
gianlucanied <gianluca.nieddu96@gmail.com>
2024-02-28 15:57:26 +0100

    Add: test backend frontend

---
+++ front-end/vue-js-api/src/components/ApiContent.vue
@@ -0,0 +1,15 @@
+<template></template>
+
+<script>
+import axios from "axios";
+
+export default {
+    name: "ApiContent",
+    mounted() {
+        axios.get("http://localhost:8000/api/v1/test").then((res) => {
+            const data = res.data;
+            console.log(data);
+        });
+    },
+};
+</script>
--- routes/web.php
+++ routes/web.php
@@ -2,25 +2,25 @@
 
 use Illuminate\Support\Facades\Route;
 
-use App\Http\Controllers\ProjectController;
+// use App\Http\Controllers\ProjectController;
 
-use App\Http\Controllers\TechnologyController;
+// use App\Http\Controllers\TechnologyController;
 
 
-Route::get('/', [ProjectController :: class, 'index'])
-    -> name('project.index');
+// Route::get('/', [ProjectController :: class, 'index'])
+//     -> name('project.index');
 
-Route::get('/projects/{id}', [ProjectController :: class, 'show'])
-    -> name('project.show');
+// Route::get('/projects/{id}', [ProjectController :: class, 'show'])
+//     -> name('project.show');
 
-Route::get('/create', [ProjectController :: class, 'create'])
-    -> name('project.create');
+// Route::get('/create', [ProjectController :: class, 'create'])
+//     -> name('project.create');
 
-Route::post('/create', [ProjectController :: class, 'store'])
--> name('project.store');
+// Route::post('/create', [ProjectController :: class, 'store'])
+// -> name('project.store');
 
-Route::get('/projects/{id}/edit', [ProjectController :: class, 'edit'])
--> name('project.edit');
+// Route::get('/projects/{id}/edit', [ProjectController :: class, 'edit'])
+// -> name('project.edit');
 
-Route::put('/projects/{id}/edit', [ProjectController :: class, 'update'])
--> name('project.update');
+// Route::put('/projects/{id}/edit', [ProjectController :: class, 'update'])
+// -> name('project.update');
